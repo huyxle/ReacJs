80055ecebba124dc6c49f64d3a8239fa898d691f
huyxle <huyxle@kms-technology.com>
2020-04-20 13:48:27 +0700

    reacjs self-learning

---
+++ reactjs_hooks/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Example from './UseEffect/UseEffectComponent';
+
+function App() {
+  return (
+    <Example />
+  );
+}
+
+export default App;
